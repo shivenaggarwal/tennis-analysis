fda3af5c904862556f768e7ab910a13a30f7ba27
shivenaggarwal <shivenagg04@gmail.com>
2025-03-12 21:09:57 +0530

    kps model

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,11 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO("models/yolo5_last.pt")
+# model = YOLO("models/yolo5_last.pt")
+model = YOLO("yolov8x")
 
-result = model.predict("input_videos/input_video.mp4", conf=0.2, save=True)
 
-print(result)
+result = model.track("input_videos/input_video.mp4", conf=0.2, save=True)
 
-print("boxes: ")
-for box in result[0].boxes:
-    print(box)
+# print(result)
+
+# print("boxes: ")
+# for box in result[0].boxes:
+#     print(box)
