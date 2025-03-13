3c85162b86643b251af667c23e006367d0ac6f9b
shivenaggarwal <shivenagg04@gmail.com>
2025-03-13 16:30:52 +0530

    init mini court visualisation

---
+++ utils/conversions.py
@@ -0,0 +1,7 @@
+def convert_pixel_distance_to_meters(pixel_distance, refrence_height_in_meters, refrence_height_in_pixels):
+    # cross multiplication
+    return (pixel_distance * refrence_height_in_meters) / refrence_height_in_pixels
+
+
+def convert_meters_to_pixel_distance(meters, refrence_height_in_meters, refrence_height_in_pixels):
+    return (meters * refrence_height_in_pixels) / refrence_height_in_meters
